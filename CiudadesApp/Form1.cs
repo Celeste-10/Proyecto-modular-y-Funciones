6d50bafc3807099fe8673b9ef4b03d01e40bf8d9
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2025-09-21 16:09:04 -0600

    Second commit

---
+++ CiudadesApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CiudadesLibrary.Dao;
+using CiudadesLibrary.Modelos;
+
+namespace CiudadesApp
+{
+    public partial class Form1 : Form
+    {
+        CiudadDao lista = new CiudadDao();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Ciudad ciudad = new Ciudad();
+            ciudad.Nombre=tbNombre.Text;
+            ciudad.poblacion = int.Parse(tbPoblacion.Text);
+            ciudad.FechaFundacion = dtpFundacion.Value;
+            lista.AgregarCiudad(ciudad);
+            llenarDataGrid();
+        }
+
+        private void llenarDataGrid()
+        {
+            dgvRegistro.DataSource = lista.MostrarCiudad();
+            dgvRegistro.Refresh();
+        }
+    }
+}
